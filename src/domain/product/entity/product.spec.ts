9c5fa6603e9bfae73c1bab49783c37503aa3c4c9
raul-hrosa <rosa.hraul@gmail.com>
2022-09-15 11:02:16 -0300

    desafio notification pattern

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -2,15 +2,19 @@ import Product from './product'
 
 describe('PRoduct unit tests', () => {
     it('should trow error when id is empty', () => {
-        expect(() => new Product("", "Product 1", 100)).toThrowError("Id is required")
+        expect(() => new Product("", "Product 1", 100)).toThrowError("product: Id is required")
     })
     
     it('should trow error when name is empty', () => {
-        expect(() => new Product("123", "", 100)).toThrowError("Name is required")
+        expect(() => new Product("123", "", 100)).toThrowError("product: Name is required")
     })
 
     it('should trow error when price is less than zero', () => {
-        expect(() => new Product("123", "Product 1", -1)).toThrowError("Price must be greater than zero")
+        expect(() => new Product("123", "Product 1", -1)).toThrowError("product: Price must be greater than zero")
+    })
+
+    it('should trow error when id and name is empty and price is less than zero', () => {
+        expect(() => new Product("", "", -1)).toThrowError("product: Id is required,product: Name is required,product: Price must be greater than zero")
     })
 
     it('should change name', () => {
